0681fccece7c3c0dc92501cabba3557fdd6c20fb
aaa <aaa@aaa.com>
2018-05-15 12:15:25 +0300

    repo2sfs_custom v001 , slimjet

--- slimjet_repo2sfs/init_slimjet_repo2sfs.sh
+++ slimjet_repo2sfs/init_slimjet_repo2sfs.sh
@@ -2,9 +2,9 @@
 echo "INFO TEXT : when adked for EXTRA packages enter libnss3 libgconf-2-4"
 MYFILENAME=slimjet_amd64.deb
 MYVERSION=_18.0.5.0_20180426_repo2sfs_v01
-wget https://www.slimjetbrowser.com/release/$MYFILENAME.deb
+wget https://www.slimjetbrowser.com/release/$MYFILENAME
 #dpkg-deb -e $MYFILENAME.deb		#extract the DEBIAN folder to the current directory
 #mkdir -p /tmp/repo2sfs
 #cp DEBIAN/control /tmp/repo2sfs/$MYFILENAME_control.txt
-../repo2sfs $MYFILENAME.deb
-mv $MYFILENAME.squashfs $MYFILENAME_$MYVERSION.squashfs
+../repo2sfs_custom $MYFILENAME
+#mv $MYFILENAME.squashfs $MYFILENAME_$MYVERSION.squashfs
