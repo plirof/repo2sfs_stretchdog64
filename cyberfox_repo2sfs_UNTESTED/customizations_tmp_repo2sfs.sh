c6a74b9bcd705487435e7cb0f19be5a1da5894bf
aaa <aaa@aaa.com>
2018-05-15 15:28:07 +0300

    cyberfox - added firefox default path scripts sda1 ,sdb1

--- cyberfox_repo2sfs_UNTESTED/customizations_tmp_repo2sfs.sh
+++ cyberfox_repo2sfs_UNTESTED/customizations_tmp_repo2sfs.sh
@@ -8,7 +8,9 @@ echo "ERROR firefox based - CYBERFOX browser CUSTOMIZATION SCRIPT"
 #latest cyberfox amd64 https://www.cyberfox.com/el/computer/linux
 mkdir -p /tmp/repo2sfs/usr/bin
 # needs extras: libnss3 libgconf-2-4
-
+/tmp/repo2sfs/usr/bin/
+cp -n ./cyberfox_default_sda1_prefs.js /tmp/repo2sfs/usr/bin/cyberfox_default_sda1_prefs.js
+cp -n ./cyberfox_default_sdb1_prefs.js /tmp/repo2sfs/usr/bin/cyberfox_default_sdb1_prefs.js
 
 #!/bin/sh
 
@@ -17,9 +19,9 @@ cat > /tmp/repo2sfs/usr/bin/cyberfox-puppy << EOF
 #export CHROMIUM_FLAGS="--ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
 #export cyberfox_FLAGS="--ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so  --ppapi-flash-version=29.0.0.171  --media-cache-size=10000000"
 xhost +local:puppy
-#su - puppy -c "/usr/bin/cyberfox  --user-data-dir=/home/puppy/cyberfox_puppy_user_data_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
 su -l puppy -c "cyberfox --user-data-dir=/home/puppy/.data/cyberfox_puppy_user_data_dir --disk-cache-dir=/home/puppy/.cache/cyberfox_puppy_user_cache_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so  --ppapi-flash-version=29.0.0.171 --disable-translate --always-authorize-plugins --media-cache-size=10000000 \$1"
 EOF
+
 chmod 755 /tmp/repo2sfs/usr/bin/cyberfox-puppy
 
 cat > /tmp/repo2sfs/usr/share/applications/cyberfox_changesdat.desktop << EOF2
@@ -44,9 +46,12 @@ cat > /tmp/repo2sfs/usr/bin/cyberfox-puppy-sda1 << EOF31
 xhost +local:puppy
 mkdir -p /mnt/sda1/downloads_linux/.data/cyberfox
 mkdir -p /mnt/sda1/downloads_linux/.cache/cyberfox
+cp -n /usr/bin/cyberfox_default_sda1_prefs.js /mnt/sda1/downloads_linux/.data/cyberfox/prefs.js
+
 #su - puppy -c "/usr/bin/cyberfox  --user-data-dir=/home/puppy/cyberfox_puppy_user_data_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
-su -l puppy -c "cyberfox --user-data-dir=/mnt/sda1/downloads_linux/.data/cyberfox --disk-cache-dir=/mnt/sda1/downloads_linux/.cache/cyberfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+su -l puppy -c 'cyberfox -profile "/mnt/sda1/downloads_linux/.data/cyberfox" \$1'
 EOF31
+
 chmod 755 /tmp/repo2sfs/usr/bin/cyberfox-puppy-sda1
 
 cat > /tmp/repo2sfs/usr/share/applications/cyberfox-puppy-sda1.desktop << EOF32
@@ -73,8 +78,11 @@ cat > /tmp/repo2sfs/usr/bin/cyberfox-puppy-sdb1 << EOF41
 xhost +local:puppy
 mkdir -p /mnt/sdb1/downloads_linux/.data/cyberfox
 mkdir -p /mnt/sdb1/downloads_linux/.cache/cyberfox
+cp -n /usr/bin/cyberfox_default_sdb1_prefs.js /mnt/sdb1/downloads_linux/.data/cyberfox/prefs.js
 #su - puppy -c "/usr/bin/cyberfox  --user-data-dir=/home/puppy/cyberfox_puppy_user_data_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
-su -l puppy -c "cyberfox --user-data-dir=/mnt/sdb1/downloads_linux/.data/cyberfox --disk-cache-dir=/mnt/sdb1/downloads_linux/.cache/cyberfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+#su -l puppy -c "cyberfox --user-data-dir=/mnt/sdb1/downloads_linux/.data/cyberfox --disk-cache-dir=/mnt/sdb1/downloads_linux/.cache/cyberfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+su -l puppy -c 'cyberfox -profile "/mnt/sdb1/downloads_linux/.data/cyberfox" \$1'
+
 EOF41
 chmod 755 /tmp/repo2sfs/usr/bin/cyberfox-puppy-sdb1
 
