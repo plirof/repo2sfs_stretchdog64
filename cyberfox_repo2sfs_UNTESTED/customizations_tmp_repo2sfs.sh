dff9affa2c74f96afae4c0df195c27177237adfd
aaa <aaa@aaa.com>
2018-05-15 13:33:54 +0300

    test Cyberfox- firefox clone

---
+++ cyberfox_repo2sfs_UNTESTED/customizations_tmp_repo2sfs.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+echo "ERROR firefox based - CYBERFOX browser CUSTOMIZATION SCRIPT"
+#############
+# cyberfox CUSTOMIZATION SCRIPT  jonmod 180514a 
+#############
+chmod 4755 /tmp/repo2sfs/usr/lib/x86_64-linux-gnu/cyberfox/cyberfox_sandbox
+#jonmod 180514a
+#latest cyberfox amd64 https://www.cyberfox.com/el/computer/linux
+mkdir -p /tmp/repo2sfs/usr/bin
+# needs extras: libnss3 libgconf-2-4
+
+
+#!/bin/sh
+
+cat > /tmp/repo2sfs/usr/bin/cyberfox-puppy << EOF
+#!/bin/sh
+#export CHROMIUM_FLAGS="--ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
+#export cyberfox_FLAGS="--ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so  --ppapi-flash-version=29.0.0.171  --media-cache-size=10000000"
+xhost +local:puppy
+#su - puppy -c "/usr/bin/cyberfox  --user-data-dir=/home/puppy/cyberfox_puppy_user_data_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
+su -l puppy -c "cyberfox --user-data-dir=/home/puppy/.data/cyberfox_puppy_user_data_dir --disk-cache-dir=/home/puppy/.cache/cyberfox_puppy_user_cache_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so  --ppapi-flash-version=29.0.0.171 --disable-translate --always-authorize-plugins --media-cache-size=10000000 \$1"
+EOF
+chmod 755 /tmp/repo2sfs/usr/bin/cyberfox-puppy
+
+cat > /tmp/repo2sfs/usr/share/applications/cyberfox_changesdat.desktop << EOF2
+[Desktop Entry]
+Version=1.0
+Name=cyberfox puppy -repo2sfs changes.dat
+Exec=cyberfox-puppy
+Terminal=false
+X-MultipleArgs=false
+Type=Application
+Icon=/usr/share/pixmaps/cyberfox.xpm
+Categories=Network;
+MimeType=text/html;text/xml;application/xhtml_xml;x-scheme-handler/http;x-scheme-handler/https;
+StartupWMClass=cyberfox
+StartupNotify=true
+EOF2
+
+
+cat > /tmp/repo2sfs/usr/bin/cyberfox-puppy-sda1 << EOF31
+#!/bin/sh
+
+xhost +local:puppy
+mkdir -p /mnt/sda1/downloads_linux/.data/cyberfox
+mkdir -p /mnt/sda1/downloads_linux/.cache/cyberfox
+#su - puppy -c "/usr/bin/cyberfox  --user-data-dir=/home/puppy/cyberfox_puppy_user_data_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
+su -l puppy -c "cyberfox --user-data-dir=/mnt/sda1/downloads_linux/.data/cyberfox --disk-cache-dir=/mnt/sda1/downloads_linux/.cache/cyberfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+EOF31
+chmod 755 /tmp/repo2sfs/usr/bin/cyberfox-puppy-sda1
+
+cat > /tmp/repo2sfs/usr/share/applications/cyberfox-puppy-sda1.desktop << EOF32
+[Desktop Entry]
+Version=1.0
+Name=cyberfox puppy -sda1(C:) EXTERNAL repo2sfs
+Exec=cyberfox-puppy-sda1
+Terminal=false
+X-MultipleArgs=false
+Type=Application
+Icon=/usr/share/pixmaps/cyberfox.xpm
+Categories=Network;
+MimeType=text/html;text/xml;application/xhtml_xml;x-scheme-handler/http;x-scheme-handler/https;
+StartupWMClass=cyberfox
+StartupNotify=true
+EOF32
+
+
+
+
+cat > /tmp/repo2sfs/usr/bin/cyberfox-puppy-sdb1 << EOF41
+#!/bin/sh
+
+xhost +local:puppy
+mkdir -p /mnt/sdb1/downloads_linux/.data/cyberfox
+mkdir -p /mnt/sdb1/downloads_linux/.cache/cyberfox
+#su - puppy -c "/usr/bin/cyberfox  --user-data-dir=/home/puppy/cyberfox_puppy_user_data_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
+su -l puppy -c "cyberfox --user-data-dir=/mnt/sdb1/downloads_linux/.data/cyberfox --disk-cache-dir=/mnt/sdb1/downloads_linux/.cache/cyberfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+EOF41
+chmod 755 /tmp/repo2sfs/usr/bin/cyberfox-puppy-sdb1
+
+cat > /tmp/repo2sfs/usr/share/applications/cyberfox-puppy-sdb1.desktop << EOF42
+[Desktop Entry]
+Version=1.0
+Name=cyberfox puppy -sdb1 (2nd DISK-USB) EXTERNAL repo2sfs
+Exec=cyberfox-puppy-sdb1
+Terminal=false
+X-MultipleArgs=false
+Type=Application
+Icon=/usr/share/pixmaps/cyberfox.xpm
+Categories=Network;
+MimeType=text/html;text/xml;application/xhtml_xml;x-scheme-handler/http;x-scheme-handler/https;
+StartupWMClass=cyberfox
+StartupNotify=true
+EOF42
+
+echo "DONE with CUSTOMIZATION of /tmp/repo2sfs ........ "
+#read -p "Done. Remember to create the puppy user. Press Enter: "
---
+++ cyberfox_repo2sfs_UNTESTED/init_cyberfox_repo2sfs.sh
@@ -0,0 +1,11 @@
+#change MYFILENAME and MYVERSION to help renaming
+echo "1st MANUALLY download opera from here : https://www.opera.com/el/computer/linux  , example link https://download1.operacdn.com/pub/opera/desktop/53.0.2907.37/linux/opera-stable_53.0.2907.37_amd64.deb "
+#echo "INFO TEXT : when adked for EXTRA packages enter libnss3 libgconf-2-4"
+MYFILENAME=Cyberfox-52.7.4.en-US.linux-x86_64.deb
+MYVERSION=_18.0.5.0_20180426_repo2sfs_v01
+#wget https://www.slimjetbrowser.com/release/$MYFILENAME
+#dpkg-deb -e $MYFILENAME.deb		#extract the DEBIAN folder to the current directory
+#mkdir -p /tmp/repo2sfs
+#cp DEBIAN/control /tmp/repo2sfs/$MYFILENAME_control.txt
+../repo2sfs_custom $1
+#mv $MYFILENAME.squashfs $MYFILENAME_$MYVERSION.squashfs
