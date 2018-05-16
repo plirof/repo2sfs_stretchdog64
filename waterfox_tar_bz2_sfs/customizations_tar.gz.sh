f552cb46217a96eadc2a7a4c112c2d39d6a58175
aaa <aaa@aaa.com>
2018-05-16 13:42:02 +0300

    renamed

--- waterfox_tar_bz2_sfs/customizations_tar.gz.sh
+++ waterfox_tar_bz2_sfs/customizations_tar.gz.sh
@@ -20,21 +20,21 @@ cp -n ./waterfox_default_sdb1_prefs.js $1/usr/bin/waterfox_default_sdb1_prefs.js
 
 
 
-cat > $1/usr/bin/waterfox-puppy << EOF
+cat > $1/usr/bin/waterfox-puppy.sh << EOF
 #!/bin/sh
 #export CHROMIUM_FLAGS="--ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --ppapi-flash-version=26.0.0.137"
 #export waterfox_FLAGS="--ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so  --ppapi-flash-version=29.0.0.171  --media-cache-size=10000000"
 xhost +local:puppy
-#su -l puppy -c "waterfox --user-data-dir=/home/puppy/.data/waterfox_puppy_user_data_dir --disk-cache-dir=/home/puppy/.cache/waterfox_puppy_user_cache_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so  --ppapi-flash-version=29.0.0.171 --disable-translate --always-authorize-plugins --media-cache-size=10000000 \$1"
-su -l puppy -c "waterfox"
+#su -l puppy -c "/opt/waterfox/waterfox --user-data-dir=/home/puppy/.data/waterfox_puppy_user_data_dir --disk-cache-dir=/home/puppy/.cache/waterfox_puppy_user_cache_dir --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so  --ppapi-flash-version=29.0.0.171 --disable-translate --always-authorize-plugins --media-cache-size=10000000 \$1"
+su -l puppy -c "/opt/waterfox/waterfox"
 EOF
-chmod 755 $1/usr/bin/waterfox-puppy
+chmod 755 $1/usr/bin/waterfox-puppy.sh
 
 cat > $1/usr/share/applications/waterfox_changesdat.desktop << EOF2
 [Desktop Entry]
 Version=1.0
 Name=waterfox puppy -repo2sfs changes.dat
-Exec=waterfox-puppy
+Exec=waterfox-puppy.sh
 Terminal=false
 X-MultipleArgs=false
 Type=Application
@@ -46,23 +46,23 @@ StartupNotify=true
 EOF2
 
 
-cat > $1/usr/bin/waterfox-puppy-sda1 << EOF31
+cat > $1/usr/bin/waterfox-puppy-sda1.sh << EOF31
 #####   SDA1 #####################################################
 
 xhost +local:puppy
 mkdir -p /mnt/sda1/downloads_linux/.data/waterfox
 mkdir -p /mnt/sda1/downloads_linux/.cache/waterfox
 cp -n /usr/bin/waterfox_default_sda1_prefs.js /mnt/sda1/downloads_linux/.data/waterfox/prefs.js
-#su -l puppy -c "waterfox --user-data-dir=/mnt/sda1/downloads_linux/.data/waterfox --disk-cache-dir=/mnt/sda1/downloads_linux/.cache/waterfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
-su -l puppy -c 'waterfox -profile "/mnt/sda1/downloads_linux/.data/waterfox" \$1'
+#su -l puppy -c "/opt/waterfox/waterfox --user-data-dir=/mnt/sda1/downloads_linux/.data/waterfox --disk-cache-dir=/mnt/sda1/downloads_linux/.cache/waterfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+su -l puppy -c '/opt/waterfox/waterfox -profile "/mnt/sda1/downloads_linux/.data/waterfox" \$1'
 EOF31
-chmod 755 $1/usr/bin/waterfox-puppy-sda1
+chmod 755 $1/usr/bin/waterfox-puppy-sda1.sh
 
 cat > $1/usr/share/applications/waterfox-puppy-sda1.desktop << EOF32
 [Desktop Entry]
 Version=1.0
 Name=waterfox puppy -sda1(C:) EXTERNAL repo2sfs
-Exec=waterfox-puppy-sda1
+Exec=waterfox-puppy-sda1.sh
 Terminal=false
 X-MultipleArgs=false
 Type=Application
@@ -76,23 +76,23 @@ EOF32
 
 
 
-cat > $1/usr/bin/waterfox-puppy-sdb1 << EOF41
+cat > $1/usr/bin/waterfox-puppy-sdb1.sh << EOF41
 #!/bin/sh
 
 xhost +local:puppy
 mkdir -p /mnt/sdb1/downloads_linux/.data/waterfox
 mkdir -p /mnt/sdb1/downloads_linux/.cache/waterfox
 cp -n /usr/bin/waterfox_default_sdb1_prefs.js /mnt/sdb1/downloads_linux/.data/waterfox/prefs.js
-#su -l puppy -c "waterfox --user-data-dir=/mnt/sdb1/downloads_linux/.data/waterfox --disk-cache-dir=/mnt/sdb1/downloads_linux/.cache/waterfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
-su -l puppy -c 'waterfox -profile "/mnt/sda1/downloads_linux/.data/waterfox" \$1'
+#su -l puppy -c "/opt/waterfox/waterfox --user-data-dir=/mnt/sdb1/downloads_linux/.data/waterfox --disk-cache-dir=/mnt/sdb1/downloads_linux/.cache/waterfox --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+su -l puppy -c '/opt/waterfox/waterfox -profile "/mnt/sda1/downloads_linux/.data/waterfox" \$1'
 EOF41
-chmod 755 $1/usr/bin/waterfox-puppy-sdb1
+chmod 755 $1/usr/bin/waterfox-puppy-sdb1.sh
 
 cat > $1/usr/share/applications/waterfox-puppy-sdb1.desktop << EOF42
 [Desktop Entry]
 Version=1.0
 Name=waterfox puppy -sdb1 (2nd DISK-USB) EXTERNAL repo2sfs
-Exec=waterfox-puppy-sdb1
+Exec=waterfox-puppy-sdb1.sh
 Terminal=false
 X-MultipleArgs=false
 Type=Application
