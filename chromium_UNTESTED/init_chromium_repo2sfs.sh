f990f5ef0b5856557604b236d6d9684540e9c1ca
aaa <aaa@aaa.com>
2018-08-31 10:09:50 +0300

    chromium tests NOT working yet

--- chromium_UNTESTED/init_chromium_repo2sfs.sh
+++ chromium_UNTESTED/init_chromium_repo2sfs.sh
@@ -2,7 +2,7 @@
 echo "1st MANUALLY download chromium from here : http://security.debian.org/debian-security/pool/updates/main/c/chromium-browser/  , https://packages.debian.org/stretch/amd64/chromium/download  , example link http://security.debian.org/debian-security/pool/updates/main/c/chromium-browser/chromium_68.0.3440.75-1~deb9u1_amd64.deb "
 #echo "INFO TEXT : when adked for EXTRA packages enter libnss3 libgconf-2-4"
 MYFILENAME=chromium_68.0.3440.75-1~deb9u1_amd64
-MYFILENAME_DRIVER=chromedriver_68.0.3440.75-1~deb9u1_amd64
+MYFILENAME_DRIVER=chromium-driver_68.0.3440.75-1~deb9u1_amd64
 #MYVERSION=_18.0.5.0_20180426_repo2sfs_v01
 echo "+++++++++++++ DOWNLOADING DEBs+++++++++++++++"
 wget  "http://security.debian.org/debian-security/pool/updates/main/c/chromium-browser/"$MYFILENAME".deb"
@@ -15,13 +15,15 @@ mkdir -p /tmp/repo2sfs/usr/bin
 mkdir -p /tmp/repo2sfs/usr/share/applications/
 #dpkg-deb -e $MYFILENAME.deb		#extract the DEBIAN folder to the current directory
 echo "+++++++++++++ HANDLING $MYFILENAME +++++++++++++++"
-dpkg-deb -e $MYFILENAME.deb		#extract the DEBIAN folder to the current directory
-cp DEBIAN/control "/tmp/repo2sfs/"$MYFILENAME"_control.txt"
+dpkg-deb -x $MYFILENAME.deb	./$MYFILENAME_UNZIP	#extract the DEBIAN folder to the current directory
+dpkg-deb -e $MYFILENAME.deb	./$MYFILENAME_UNZIP	#extract the DEBIAN folder to the current directory
+cp $MYFILENAME"_UNZIP/control" "/tmp/repo2sfs/"$MYFILENAME"_control.txt"
 
 echo "+++++++++++++ $MYFILENAME_DRIVER +++++++++++++++"
-dpkg-deb -e $MYFILENAME_DRIVER.deb		#extract the DEBIAN folder to the current directory
-cp DEBIAN/usr/bin/chromedriver "/tmp/repo2sfs/usr/bin/chromedriver"
-cp DEBIAN/control "/tmp/repo2sfs/"$MYFILENAME_DRIVER"_control.txt"
+dpkg-deb -x $MYFILENAME_DRIVER".deb"	./$MYFILENAME_DRIVER_UNZIP	#extract the DEBIAN folder to the current directory
+dpkg-deb -e $MYFILENAME_DRIVER".deb"	./$MYFILENAME_DRIVER_UNZIP	#extract the DEBIAN folder to the current directory
+cp $MYFILENAME_DRIVER"_UNZIP/usr/bin/chromedriver" "/tmp/repo2sfs/usr/bin/chromedriver"
+cp $MYFILENAME_DRIVER"_UNZIP/control" "/tmp/repo2sfs/"$MYFILENAME_DRIVER"_control.txt"
 
 echo "+++++++++++++ executing repo2sfs_custom +++++++++++++++"
 ../repo2sfs_custom $MYFILENAME.deb
