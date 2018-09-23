4d703f27f4d9c8e875066b9d429bd7563566cd6e
aaa <aaa@aaa.com>
2018-09-23 11:09:53 +0300

    palemoon added sda2 option

--- palemoon_repo2sfs/customizations_tmp_repo2sfs.sh
+++ palemoon_repo2sfs/customizations_tmp_repo2sfs.sh
@@ -8,10 +8,12 @@ echo "PALEMOON browser CUSTOMIZATION SCRIPT (FIREFOX BASED)"
 #latest palemoon amd64 https://www.palemoon.com/el/computer/linux
 # set to true (lowercase)  if you want extra scripts/.desktop created
 SDA1SCRIPTS=true
+SDA2SCRIPTS=true
 SDB1SCRIPTS=true
 
 mkdir -p /tmp/repo2sfs/usr/bin
 cp -n ./palemoon_default_sda1_prefs.js /tmp/repo2sfs/usr/bin/palemoon_default_sda1_prefs.js
+cp -n ./palemoon_default_sda2_prefs.js /tmp/repo2sfs/usr/bin/palemoon_default_sda2_prefs.js
 cp -n ./palemoon_default_sdb1_prefs.js /tmp/repo2sfs/usr/bin/palemoon_default_sdb1_prefs.js
 # needs extras: libnss3 libgconf-2-4
 
@@ -108,6 +110,37 @@ StartupNotify=true
 EOF42
 fi
 
+################### sda2 #########################################################
+if [ $SDA2SCRIPTS == true ]
+then
+cat > /tmp/repo2sfs/usr/bin/palemoon-puppy-sda2 << EOF51
+
+
+xhost +local:puppy
+mkdir -p /mnt/sda2/downloads_linux/.data/palemoon
+mkdir -p /mnt/sda2/downloads_linux/.cache/palemoon
+cp -n /usr/bin/palemoon_default_sda2_prefs.js /mnt/sda2/downloads_linux/.data/palemoon/prefs.js
+#su -l puppy -c "palemoon --user-data-dir=/mnt/sda2/downloads_linux/.data/palemoon --disk-cache-dir=/mnt/sda2/downloads_linux/.cache/palemoon --ppapi-flash-path=/usr/lib/adobe-flashplugin/libpepflashplayer.so --disable-translate --always-authorize-plugins  --ppapi-flash-version=29.0.0.171 \$1"
+su -l puppy -c 'palemoon -profile "/mnt/sda2/downloads_linux/.data/palemoon" \$1'
+EOF51
+
+chmod 755 /tmp/repo2sfs/usr/bin/palemoon-puppy-sda2
+
+cat > /tmp/repo2sfs/usr/share/applications/palemoon-puppy-sda2.desktop << EOF52
+[Desktop Entry]
+Version=1.0
+Name=palemoon puppy -sda2(C:) EXTERNAL repo2sfs
+Exec=palemoon-puppy-sda2
+Terminal=false
+X-MultipleArgs=false
+Type=Application
+Icon=/usr/share/pixmaps/palemoon.xpm
+Categories=Network;
+MimeType=text/html;text/xml;application/xhtml_xml;x-scheme-handler/http;x-scheme-handler/https;
+StartupWMClass=palemoon
+StartupNotify=true
+EOF52
+fi
 
 
 
