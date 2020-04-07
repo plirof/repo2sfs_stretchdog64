52c19c051456d490c37349919686ac3c842e4d56
aaa <aaa@aaa.com>
2020-04-07 10:15:38 +0300

    200405 - added extra mp4 &neflix libraries

--- slimjet_deb_extract/init_slimjet_extracted2sfs.sh
+++ slimjet_deb_extract/init_slimjet_extracted2sfs.sh
@@ -1,5 +1,6 @@
 #change MYFILENAME and MYVERSION to help renaming
-echo "INFO TEXT : when adked for EXTRA packages enter libnss3 libgconf-2-4"
+echo "INFO TEXT : get mp4 decoding lib from here :https://github.com/iteufel/nwjs-ffmpeg-prebuilt/releases"
+
 MYFILENAME=slimjet_amd64.deb
 
 
@@ -16,6 +17,12 @@ uextract $MYFILENAME
 #cp DEBIAN/control /tmp/repo2sfs/$MYFILENAME_control.txt
 #../repo2sfs_custom $MYFILENAME
 #mv $MYFILENAME.squashfs $MYFILENAME_$MYVERSION.squashfs
+
+# add latest libffmpeg.so for MP4
+unzip -o 0.44.1-linux-x64__chromium80.0.3987.87.zip -d $MYFILENAME.extracted/opt/slimjet/
+cp libwidevinecdm.so_191215 $MYFILENAME.extracted/opt/slimjet/libwidevinecdm.so
+
+
 
 TODAYDATE=$(date +'%y%m%d')
 echo "$MYFILENAME""_auto_v01_"$TODAYDATE".squashfs"
