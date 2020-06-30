3bf451b32dba4f736b3e056bd047d1d9fd94842e
aaa <aaa@aaa.com>
2020-06-30 10:16:48 +0300

    200630 chromium link update

--- chromium_UNTESTED/init_chromium_repo2sfs.sh
+++ chromium_UNTESTED/init_chromium_repo2sfs.sh
@@ -4,10 +4,10 @@
 
 echo "For older GLibC builds check here : (main link) https://www.chromium.org/getting-involved/download-chromium    "
 echo "Check here for latest debian build (file LAST_CHANGE inside linux_x64 folder ) : https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?generation=1585112888926871&alt=media"
-echo "eg for latest build 753130 : https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/753130/"
+echo "eg for latest build 783842 : https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/783842/"
 echo "all OS builds are here :https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html"
 #echo "INFO TEXT : when adked for EXTRA packages enter libnss3 libgconf-2-4"
-LATEST_BUILD="753130"
+LATEST_BUILD="783842"
 MYFILENAME="Linux_x64_"$LATEST_BUILD"_chrome-linux"$(date +%F)".zip"
 
 WGET_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F"$LATEST_BUILD"%2Fchrome-linux.zip?generation=1585112877406284&alt=media"
