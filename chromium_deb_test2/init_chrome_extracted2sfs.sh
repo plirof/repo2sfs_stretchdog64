9cea6dd32e28c0933b90db32ad5b5a9640129dd4
aaa <aaa@aaa.com>
2022-03-14 22:36:33 +0200

    220314

--- chromium_deb_test2/init_chrome_extracted2sfs.sh
+++ chromium_deb_test2/init_chrome_extracted2sfs.sh
@@ -6,16 +6,18 @@ MYFILENAME=chromium_99.0.4844.51-2_amd64.deb
 
 
 #first delete older files/folders
-rm -rf $MYFILENAME
+##rm -rf $MYFILENAME
 rm -rf $MYFILENAME.extracted
 # if manually downloaded comment the wget command
-wget "http://ftp.us.debian.org/debian/pool/main/c/chromium/"$MYFILENAME
+##wget "http://ftp.us.debian.org/debian/pool/main/c/chromium/"$MYFILENAME
 
+uextract libjsoncpp24_1.9.4-4_amd64.deb
 uextract $MYFILENAME
+cp -r ./libjsoncpp24_1.9.4-4_amd64.deb.extracted/* ./"$MYFILENAME.extracted"/
 ./customizations_deb_extracted.sh "$MYFILENAME.extracted"
 
 #../repo2sfs_custom $MYFILENAME
 
 TODAYDATE=$(date +'%y%m%d')
-mksquashfs $MYFILENAME.extracted "$MYFILENAME""_auto_v01_"$TODAYDATE".squashfs"
+##mksquashfs $MYFILENAME.extracted "$MYFILENAME""_auto_v01_"$TODAYDATE".squashfs"
 
